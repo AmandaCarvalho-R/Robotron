0cbadc74da51cdd4af2478bd4488a2c94ff41084
Amanda Carvalho <amanda.chrys370@hotmail.com>
2023-01-27 22:27:22 -0400

    Alterando para funcionar os outros controles

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
+const controle = document.querySelectorAll(".controle-ajuste")
 
-somar.addEventListener("click", () => {manipulaDados("somar")})
+controle.forEach ( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+       manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
+ })
 
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
-
-function manipulaDados(operacao){
-    if (operacao === "subtrair") {
-        braco.value = parseInt(braco.value) -1
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector(".controle.contador")
+    
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) -1
     }
-    else{
-        braco.value = parseInt(braco.value) + 1
+    else {
+        peca.value = parseInt(peca.value) + 1
     }
 }
